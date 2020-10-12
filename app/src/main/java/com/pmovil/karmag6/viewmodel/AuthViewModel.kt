0796b476a566f9d4fa16fc5744b4f6c4b4ca8c5c
Juan Rambal <juanp231100@gmail.com>
2020-10-12 11:43:01 -0500

    feat: logout and get current user

--- app/src/main/java/com/pmovil/karmag6/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/pmovil/karmag6/viewmodel/AuthViewModel.kt
@@ -1,8 +1,10 @@
 package com.pmovil.karmag6.viewmodel
 
 import UsersRepository
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.auth.FirebaseUser
 import com.pmovil.karmag6.model.User
 import com.pmovil.karmag6.repository.firebase.auth.SessionsRepository
 import kotlinx.coroutines.launch
@@ -12,6 +14,10 @@ import java.util.*
 class AuthViewModel : ViewModel() {
     private val authRepository = SessionsRepository()
     private val userRepository = UsersRepository()
+
+    fun getCurrentUser(): LiveData<FirebaseUser> {
+        return authRepository.getUser()
+    }
 
     fun login(email: String, password: String) {
         viewModelScope.launch {
@@ -23,11 +29,20 @@ class AuthViewModel : ViewModel() {
         viewModelScope.launch {
             val authData = authRepository.register(email, password)
             if (authData != null) {
-                userRepository.create(User(name, lastname, 2, "${authData.email}", true, "${Date.from(
-                    Instant.now())}"))
-            }else{
+                userRepository.create(
+                    User(
+                        name, lastname, 2, "${authData.email}", true, "${Date.from(
+                            Instant.now()
+                        )}"
+                    )
+                )
+            } else {
                 throw Error("Auth Error")
             }
         }
+    }
+
+    fun logout() {
+        authRepository.logout()
     }
 }
